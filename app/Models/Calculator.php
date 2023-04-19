f15c574ff2a2e0d99e00fcfdb896dd8a4ba0cc7b
Kevin Caliboso <ka.caliboso@taktylstudios.com>
2023-04-19 23:07:16 +0800

    implemented subtract on Calculator class

--- app/Models/Calculator.php
+++ app/Models/Calculator.php
@@ -14,12 +14,21 @@ class Calculator
     private function convert($numbers)
     {
         $stringArray = explode(',', $numbers);
-        return array_map('intval', $stringArray);
+        return array_map('floatval', $stringArray);
     }
 
     public function add()
     {
         return array_sum($this->numbers);
+    }
+
+    public function subtract()
+    {
+        $result = $this->numbers[0];
+        for ($i = 1; $i < count($this->numbers); $i++) {
+            $result -= $this->numbers[$i];
+        }
+        return $result;
     }
 
 }
